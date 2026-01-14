a3e14701bd56fa2b57deb1c8e228578e9eb3b586
rosegaipnazarova <rosegaipnazarova@gmail.com>
2026-01-14 12:10:01 +0500

    task

---
+++ naksu.js
@@ -0,0 +1,43 @@
+//n1
+
+// findlongestword
+
+// function flw(arr) {
+//     let max=arr[0].length
+//     for (const i of arr) {
+//         if (i.length>max) {
+//             max=i
+//         }
+//     }
+//     return max
+// }
+// console.log(flw(["let","meeeeen","u"]));
+
+
+//n2
+
+//umumiy elements
+
+// function abc(arr1,arr2) {
+//     let newArr=[]
+//     for (let i = 0; i < arr1.length; i++) {
+//         const element = arr1[i]
+//         if (arr2.includes(element)===true) {
+//             newArr.push(element)
+//         }
+//     }
+//     return newArr
+// }
+// console.log(abc([1,2,3],[4,3,2]));
+
+
+
+//n3
+
+//unique
+
+// const uniqueNumbers = new Set(["olma","anor","olma"]);
+
+// console.log(uniqueNumbers); 
+
+
