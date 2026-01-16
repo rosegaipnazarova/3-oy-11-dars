2f00475c38e0e789970375b8f94bf27fd67ca004
rosegaipnazarova <rosegaipnazarova@gmail.com>
2026-01-16 10:33:56 +0500

    task

--- naksu.js
+++ naksu.js
@@ -49,6 +49,6 @@
 
 // function formatPhNum(arr) {
 //     let newStr=""
-//     return newStr="("+arr[0]+arr[1]+arr[2]+")"+" "+arr[3]+arr[4]+arr[5]+"-"+arr[6]+arr[7]+arr[8]
+//     return newStr="("+arr[0]+arr[1]+arr[2]+")"+" "+arr[3]+arr[4]+arr[5]+"-"+arr[6]+arr[7]+arr[8]+arr[9]
 // }
 // console.log(formatPhNum([1,2,3,4,5,6,7,8,9,0]));
